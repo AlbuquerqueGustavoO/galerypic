eaa1646f6178896aec29496c8bcc26f9901805ee
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-12-20 16:22:04 -0300

    finalizando modulo

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -2,23 +2,19 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
-  selector: 'ap-photos',
+  selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
 export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
-  rows = [];
+  rows: any[] = [];
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes.photos)
-    this.rows = this.groupColumns(this.photos);
-  }
-
-  ngOnInit(): void {
-    this.rows = this.groupColumns(this.photos);
+    if (changes.photos)
+      this.rows = this.groupColumns(this.photos);
   }
 
   groupColumns(photos: Photo[]) {
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
-const API = 'http://localhost:3000'
+const API = 'http://localhost:3000';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
@@ -10,11 +10,11 @@ export class PhotoService {
     constructor(private http: HttpClient) { }
 
     listFromUser(userName: string) {
-        return this.http.get<Photo[]>(API + '/' + userName + '/photos')
+        return this.http.get<Photo[]>(API + '/' + userName + '/photos');
     }
     listFromUserPaginated(userName: string, page: number) {
         const params = new HttpParams()
             .append('page', page.toString())
-        return this.http.get<Photo[]>(API + '/' + userName + '/photos', { params })
+        return this.http.get<Photo[]>(API + '/' + userName + '/photos', { params });
     }
 }
