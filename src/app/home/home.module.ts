0cae9fe194df25f0878de8df036b21c806e33b43
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-12-21 14:52:26 -0300

    inserindo navegacao pos validacao de usuario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { SigninComponent } from "./signin/signin.component";
 import {ReactiveFormsModule} from '@angular/forms'
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../photos/shared/components/vmessage/vmessage.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [SigninComponent],
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ]
 })
 
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -9,13 +12,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
-    })
+    });
   }
 
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => this.router.navigate(['user', userName]),
+        err => {
+          console.log(err);
+          this.loginForm.reset();
+          alert('login inválido, erro em usuario ou senha');
+        }
+      );
+  }
 }
