13a81a65bc32e298c9e99f6679ec7da58c870d0e
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-12-22 14:07:49 -0300

    manipulando token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { SigninComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
     {
         path: '',
-        component: SigninComponent
+        component: SigninComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'user/:userName',
