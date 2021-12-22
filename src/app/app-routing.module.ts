30f9b124e3d061050a3578a215a7effbd51702d7
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-12-22 15:01:58 -0300

    validacao form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { SigninComponent } from './home/signin/signin.component';
 import { AuthGuard } from './core/auth/auth.guard';
+import { SignUpComponent } from './home/signup/signup.component';
 
 const routes: Routes = [
     {
@@ -14,6 +15,10 @@ const routes: Routes = [
         component: SigninComponent,
         canActivate: [AuthGuard]
     },
+    {
+        path: 'signup',
+        component: SignUpComponent,
+    },    
     {
         path: 'user/:userName',
         component: PhotoListComponent,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from "@angular/core";
 import { SigninComponent } from "./signin/signin.component";
-import {ReactiveFormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../photos/shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignUpComponent } from "./signup/signup.component";
 
 @NgModule({
-    declarations: [SigninComponent],
+    declarations: [
+        SigninComponent,
+        SignUpComponent
+    ],
     imports: [
         CommonModule,
         ReactiveFormsModule,
+        FormsModule,
         VMessageModule,
         RouterModule
     ]
